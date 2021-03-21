a0d923cdc00743c6f01da869fe819a817fd9a2c6
Microsoft Tech Club LPS <45787667+MTCLPS@users.noreply.github.com>
2021-03-21 08:36:37 +0100

    History page fix

--- HIC/AssemblyInfo.cs
+++ HIC/AssemblyInfo.cs
@@ -1,3 +1,9 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Mont-400")]
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Mont-700")]
+[assembly: ExportFont("Montserrat-ExtraBold.ttf", Alias = "Mont-800")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Mont-500")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Mont-600")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
